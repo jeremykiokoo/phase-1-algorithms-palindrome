7f69d06a6f3c3c7479b043737d25b2f2b6c044c3
jeremykiokoo <jeremy.kioko@student.moringaschool.com>
2023-03-30 16:05:36 +0300

    completed

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const arrayOfLetters = word.split('');
+  const reverseLetters = arrayOfLetters.reverse()
+  const reverseString  = reverseLetters.join('')
+
+  if (word == reverseString){
+
+    return true
+
+  } else {
+    return false
+  }
+
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+
+  console.log('Expecting: true')
+  console.log('=>', isPalindrome('abba'));
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
